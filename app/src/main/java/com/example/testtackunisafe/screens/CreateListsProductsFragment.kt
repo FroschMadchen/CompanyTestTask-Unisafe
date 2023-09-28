329d2022cae80834c26d1b35884b3c6f076e33dd
Kristina Dimitrieva <98698205+FroschMadchen@users.noreply.github.com>
2023-09-28 20:25:21 +0300

    adding functionality CreateListsProductsFragment.kt

--- app/src/main/java/com/example/testtackunisafe/screens/CreateListsProductsFragment.kt
+++ app/src/main/java/com/example/testtackunisafe/screens/CreateListsProductsFragment.kt
@@ -5,15 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
 import com.example.testtackunisafe.R
+import com.example.testtackunisafe.RetrofitClient
+import com.example.testtackunisafe.databinding.FragmentAdditionProductBinding
+import com.example.testtackunisafe.databinding.FragmentCreateListsProductsBinding
 
 
 class CreateListsProductsFragment : Fragment() {
+    private var _binding:FragmentCreateListsProductsBinding? = null
+    private val mBinding get() = _binding!!
+    lateinit var navController: NavController
+    lateinit var additionProductBinding: FragmentAdditionProductBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_lists_products, container, false)
+        _binding = FragmentCreateListsProductsBinding.inflate(layoutInflater, container, false)
+        navController.navigate(R.id.action_createListsProducts_to_additionProduct)
+        val mainApi = RetrofitClient.mainApi
+
+        return mBinding.root
     }
 }
--- app/src/main/java/com/example/testtackunisafe/screens/EntranceFragment.kt
+++ app/src/main/java/com/example/testtackunisafe/screens/EntranceFragment.kt
@@ -1,6 +1,7 @@
 package com.example.testtackunisafe.screens
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,37 +11,56 @@ import androidx.navigation.NavController
 import com.example.testtackunisafe.R
 import com.example.testtackunisafe.RetrofitClient
 import com.example.testtackunisafe.databinding.FragmentEntranceBinding
+import com.example.testtackunisafe.`interface`.MainApi
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 
 class EntranceFragment : Fragment() {
-    private var _binding:FragmentEntranceBinding?=null
+    private var _binding: FragmentEntranceBinding? = null
     private val mBinding get() = _binding!!
     lateinit var navController: NavController
-    lateinit var button: Button
+    lateinit var createListsProductsFragment: CreateListsProductsFragment
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentEntranceBinding.inflate(layoutInflater,container,false)
+        _binding = FragmentEntranceBinding.inflate(layoutInflater, container, false)
 
         navController.navigate(R.id.action_entrance_to_createListsProducts)
-        val mainApi =RetrofitClient.mainApi
+        val mainApi = RetrofitClient.mainApi
 
         _binding!!.startBtn.setOnClickListener {
-                CoroutineScope(Dispatchers.IO).launch {
-
-
+            CoroutineScope(Dispatchers.IO).launch {
+                val keyValue = createTestKey(mainApi)
+                Log.i("Key value", "" + keyValue)
+                val successfulLogin: Boolean = authentication(mainApi, keyValue)
+                Log.i("SuccessfulLogin", "Successful login: " + successfulLogin)
+                requireActivity().runOnUiThread {
+                    val bundle = Bundle()
+                    bundle.putString("key", keyValue)
+                    createListsProductsFragment.arguments = bundle
+                    navController.navigate(R.id.action_entrance_to_createListsProducts)
                 }
-
-
-            navController.navigate(R.id.action_entrance_to_createListsProducts)
+            }
         }
         // Inflate the layout for this fragment
         return mBinding.root
     }
-
-
+    private suspend fun authentication(mainApi: MainApi, keyValue: String): Boolean {
+        val success = mainApi.authentication(keyValue = keyValue)
+        return success.success
+    }
+    private suspend fun createTestKey(mainApi: MainApi): String {
+        val key = mainApi.createTestKey()
+        return if (key.isSuccessful) {
+            val keyData = key.body().toString() // Get the response body as a string
+            Log.i("String value", (keyData ?: "key").toString())
+            return keyData
+        } else {
+            "null"
+        }
+    }
 }
+
